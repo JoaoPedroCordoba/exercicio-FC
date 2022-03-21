9cf4871555c814a42fdb9988a04ea1b2afe2e751
JoaoPedroCordoba <jp.8pedro86@gmail.com.br>
2022-03-21 02:35:19 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double F, C;
+
+Console.WriteLine("Digite a temperatura em °F, por favor:");
+F = Convert.ToDouble(Console.ReadLine());
+
+C = (F-32) /1.8;
+
+Console.WriteLine($"Temperatura em °C é {C:N2}°C");
